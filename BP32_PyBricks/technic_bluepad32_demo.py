04470783194cf0c6f308f4e06d273cc8e840b2fe
Jeroen Op de Beek <59441635+Jeroenodb@users.noreply.github.com>
2023-05-31 12:08:08 +0200

    inventor example gamepad read

--- BP32_PyBricks/technic_bluepad32_demo.py
+++ BP32_PyBricks/technic_bluepad32_demo.py
@@ -11,12 +11,10 @@ p=PUPDevice(Port.A)
 def read_gamepad():
     a=p.read(0)
     outp=ustruct.unpack('6h',ustruct.pack('12b',*a))
-    print(outp)
     return outp
 
 def led(nr,r,g,b):
     p.write(0,(0,0,0,0,0,0,0,0,nr,r,g,b))
-    read_gamepad()
 
 def showled():
     p.write(0,(0,0,0,0,0,0,0,0,65,0,0,0))
@@ -27,12 +25,14 @@ def servo(nr,pos):
 
 
 while True:
+
     for l in range(6):
         for i in range(6):
             led(i,0,0,0)
             #wait(2)
         led(l,30,0,0)
         showled()
+        print(read_gamepad())
         wait(20)    
     for l in range(5,1,-1):
         for i in range(6):
@@ -40,4 +40,5 @@ while True:
             #wait(2)
         led(l,30,0,0)
         showled()
+        print(read_gamepad())
         wait(20)
