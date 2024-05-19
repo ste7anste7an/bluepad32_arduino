1f954a4cfb3b925a142b2c64f7efa4b0bbdf0bce
Stefan Op de Beek <ste7an@gmail.com>
2024-05-20 00:56:30 +0200

    for testing

---
+++ Tests/MicroPython/color_all_modes.py
@@ -0,0 +1,204 @@
+from lpf2 import LPF2, DATA16, ABSOLUTE, DATA8
+from time import ticks_ms, time, sleep_ms
+import struct
+
+light_sensor_modes = [
+    {
+        "symbol": "IDX",
+        "format": {"datasets": 1, "type": 0, "figures": 2, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "COLOR",
+        "pct": (0.0, 100.0),
+        "map_in": 228,
+        "si": (0.0, 10.0),
+        "raw": (0.0, 10.0),
+    },
+    {
+        "symbol": "PCT",
+        "format": {"datasets": 1, "type": 0, "figures": 3, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "REFLT",
+        "pct": (0.0, 100.0),
+        "map_in": 48,
+        "si": (0.0, 100.0),
+        "raw": (0.0, 100.0),
+    },
+    {
+        "symbol": "PCT",
+        "format": {"datasets": 1, "type": 0, "figures": 3, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "AMBI",
+        "pct": (0.0, 100.0),
+        "map_in": 48,
+        "si": (0.0, 100.0),
+        "raw": (0.0, 100.0),
+    },
+    {
+        "symbol": "PCT",
+        "format": {"datasets": 3, "type": 0, "figures": 3, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x05\x04",
+        "map_out": 16,
+        "name": "LIGHT",
+        "pct": (0.0, 100.0),
+        "map_in": 0,
+        "si": (0.0, 100.0),
+        "raw": (0.0, 100.0),
+    },
+    {
+        "symbol": "RAW",
+        "format": {"datasets": 2, "type": 1, "figures": 4, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "RREFL",
+        "pct": (0.0, 100.0),
+        "map_in": 16,
+        "si": (0.0, 1024.0),
+        "raw": (0.0, 1024.0),
+    },
+    {
+        "symbol": "RAW",
+        "format": {"datasets": 4, "type": 1, "figures": 4, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "RGB I",
+        "pct": (0.0, 100.0),
+        "map_in": 16,
+        "si": (0.0, 1024.0),
+        "raw": (0.0, 1024.0),
+    },
+    {
+        "symbol": "RAW",
+        "format": {"datasets": 3, "type": 1, "figures": 4, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "HSV",
+        "pct": (0.0, 100.0),
+        "map_in": 16,
+        "si": (0.0, 360.0),
+        "raw": (0.0, 360.0),
+    },
+    {
+        "symbol": "RAW",
+        "format": {"datasets": 4, "type": 1, "figures": 4, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "SHSV",
+        "pct": (0.0, 100.0),
+        "map_in": 16,
+        "si": (0.0, 360.0),
+        "raw": (0.0, 360.0),
+    },
+    {
+        "symbol": "RAW",
+        "format": {"datasets": 4, "type": 1, "figures": 4, "decimals": 0},
+        "capability": b"@\x00\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "DEBUG",
+        "pct": (0.0, 100.0),
+        "map_in": 16,
+        "si": (0.0, 65535.0),
+        "raw": (0.0, 65535.0),
+    },
+    {
+        "symbol": "",
+        "format": {"datasets": 7, "type": 1, "figures": 5, "decimals": 0},
+        "capability": b"@@\x00\x00\x04\x84",
+        "map_out": 0,
+        "name": "CALIB",
+        "pct": (0.0, 100.0),
+        "map_in": 0,
+        "si": (0.0, 65535.0),
+        "raw": (0.0, 65535.0),
+    },
+]
+light_sensor_id = 61
+
+
+def mode_convert(lms_modes):
+    result = []
+    for lms_mode in lms_modes:
+        result.append(
+            LPF2.mode(
+                name=lms_mode["name"].encode("UTF-8"), #+ b"\x00"+ lms_mode["capability"],
+                size=lms_mode["format"]["datasets"],
+                data_type=lms_mode["format"]["type"],
+                writable=1 if lms_mode["map_out"] else 0,
+                format=f"{lms_mode['format']['figures']}.{lms_mode['format']['decimals']}",
+                raw_range=lms_mode["raw"],
+                percent_range=lms_mode["pct"],
+                si_range=lms_mode["si"],
+                symbol=lms_mode["symbol"],
+                functionmap=[lms_mode["map_in"], lms_mode["map_out"]],
+                view=True,
+            )
+        )
+    return result
+
+
+single_mode_ls = [
+    LPF2.mode(
+        "GAMEPAD",
+        6,
+        DATA16,
+        format="5.0",
+        symbol="XYBD",
+        raw_range=(0.0, 512.0),
+        percent_range=(0.0, 1024.0),
+        si_range=(0.0, 512.0),
+    )
+]
+
+single_mode_ds = [
+    LPF2.mode(
+        "DISTL",
+        1,
+        DATA16,
+        format="5.1",
+        symbol="CM",
+        raw_range=(0.0, 250.0),
+        percent_range=(0.0, 100.0),
+        si_range=(0.0, 2500.0),
+        functionmap=[0, 145],
+    )
+]
+
+
+
+sensor_emu = LPF2(mode_convert(light_sensor_modes), 61, debug=False)# ,rx=8,tx=7) # Connects but no hearbeat.
+sensor_emu.current_mode=5
+
+d = 0
+c=0
+old_mode=0
+while 1:
+    c+=1
+    if sensor_emu.current_mode!=old_mode:
+        old_mode=sensor_emu.current_mode
+        print("mode changed to ",old_mode)
+    if c>50:
+        c=0
+        d += 1
+        d%=1024
+    if sensor_emu.current_mode==0: # idx
+        sensor_emu.send_payload(struct.pack("B", d%10))
+    elif sensor_emu.current_mode==1: # reflx
+        sensor_emu.send_payload(struct.pack("B", 10+d%10))
+    elif sensor_emu.current_mode==2: # ambi
+        sensor_emu.send_payload(struct.pack("B", 20+d%10))
+    elif sensor_emu.current_mode==3: # light
+        sensor_emu.send_payload(struct.pack("3B", 30+d%10,31+d%10,32+d%10))
+    elif sensor_emu.current_mode==4: #     RREFL
+        sensor_emu.send_payload(struct.pack("2H", d+100,d+101))
+    elif sensor_emu.current_mode==5:
+        sensor_emu.send_payload(struct.pack("4H", d,d+256,d+384,d+512))
+    elif sensor_emu.current_mode==7:
+        sensor_emu.send_payload(struct.pack("4H", d+1,d+2,d+3,d+4))
+        
+    data_in = sensor_emu.heartbeat()
+    #print(d)
+    if data_in:
+        print(f"\nReceived: {data_in[0]} on mode {data_in[1]}")
+    sleep_ms(50)
